09668ff2b513d1a397eecd01029bad424c568980
sra2009 <syedresadali@gmail.com>
2026-02-16 20:44:36 +0530

    Create src/components/Footer.tsx file

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="py-10 bg-gray-900 text-gray-400 text-center">
+      <div className="container mx-auto px-6">
+        <p>© 2026 AI Learn. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
