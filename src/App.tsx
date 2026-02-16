789eaa979666083ea059f8ad846152f03a58b654
sra2009 <syedresadali@gmail.com>
2026-02-16 20:39:59 +0530

    Create src/App.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Navigation from "@/components/Navigation";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import HowItWorks from "@/components/HowItWorks";
+import Testimonials from "@/components/Testimonials";
+import Pricing from "@/components/Pricing";
+import CTA from "@/components/CTA";
+import Footer from "@/components/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navigation />
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Testimonials />
+      <Pricing />
+      <CTA />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
